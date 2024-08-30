7a3de290ed6d2e9dfcc6224ccf723885b3d1ad29
GerardAlba <gerard.alba@gmail.com>
2024-08-30 17:56:34 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -33,10 +33,10 @@ class ChatSession:
 
     completions = {
             1:dict(
-                completion=openai.ChatCompletion,model="gpt-3.5-turbo",text='message.content',prompt='messages'
+                completion=openai.ChatCompletion,model="davinci-002",text='message.content',prompt='messages'
             ),
             0:dict(
-                completion=openai.Completion,model="text-davinci-003",text='text',prompt='prompt'
+                completion=openai.Completion,model="davinci-002",text='text',prompt='prompt'
             )
         }
     
